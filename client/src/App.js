0a7250d176478ee7b46deddb788c6a8399bd6d8d
Jacob Walton <jawalton6616@gmail.com>
2020-08-05 18:39:05 -0500

    adjusted theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import { createMuiTheme } from "@material-ui/core/styles";
-import green from "@material-ui/core/colors/green";
-import red from "@material-ui/core/colors/red";
+// import green from "@material-ui/core/colors/green";
+// import red from "@material-ui/core/colors/red";
 import { BrowserRouter } from "react-router-dom";
 
 import Routes from "./Routes";
@@ -10,21 +10,27 @@ import Routes from "./Routes";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: "#C8E6C9",
-      main: "#4CAF50",
-      dark: "#2E7D32",
-      contrastText: "#fff"
+      light: "#7ef1da",
+      main: "#1D8B75",
+      dark: "#135245",
+      contrastText: "#fff",
     },
     secondary: {
-      light: "#EF9A9A",
-      main: "#F44336",
-      dark: "#C62828",
-      contrastText: "#000"
+      light: "#d5ff74",
+      main: "#6B8C1D",
+      dark: "#3b5207",
+      contrastText: "#000",
     },
-    openTitle: green["400"],
-    protectTitle: red["400"],
-    type: "dark"
-  }
+    danger: {
+      light: "#f56985",
+      main: "#8C1D34",
+      dark: "#580415",
+      contrastText: "#000",
+    },
+    openTitle: "#1D8B75",
+    protectTitle: "#1D8B75",
+    type: "dark",
+  },
 });
 
 class App extends Component {
