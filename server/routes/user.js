11d14e3f3894a3eb91b1f82bff2986a1213aa8d2
Andrew Hansbrough <hansbroughku@gmail.com>
2020-08-04 20:38:35 -0500

    Adding Server config, controlelrs, helpers, models, and routes for authentication

---
+++ server/routes/user.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  registerUser,
+  findUserById,
+  findUserProfile,
+  deleteUser,
+} from "../controllers/user.js";
+
+// import them to protect routes
+import { requireSignin, hasAuthorization } from "../controllers/auth.js";
+
+const router = express.Router();
+
+router.route("/api/users").post(registerUser);
+
+router
+  .route("/api/users/:userId")
+  .get(requireSignin, findUserProfile)
+  .delete(requireSignin, hasAuthorization, deleteUser);
+
+router.param("userId", findUserById);
+
+export default router;
