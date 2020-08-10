c5a4fc042c1021ff733acab8ddf5c3130b96fdc3
Joe <joeythekid031@gmail.com>
2020-08-09 19:51:03 -0500

    fixed an error on pullchart

--- client/src/components/PollChart/PollChart.js
+++ client/src/components/PollChart/PollChart.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 import Paper from "@material-ui/core/Paper";
 import {
-  Chart,
-  PieSeries,
-  Title,
-  Legend
+    Chart,
+    PieSeries,
+    Title,
+    Legend
 } from "@devexpress/dx-react-chart-material-ui";
 
 import { Animation } from "@devexpress/dx-react-chart";
@@ -12,38 +12,38 @@ import { Animation } from "@devexpress/dx-react-chart";
 // Data should be pulled from our backend
 
 const data = [
-  { optionTitle: "Someone", optionVal: 6 },
-  { optionTitle: "Joe", optionVal: 6 },
-  { optionTitle: "Jacob", optionVal: 6 },
-  { optionTitle: "Andrew", optionVal: 6 },
-  { optionTitle: "Angel", optionVal: 6 }
+    { optionTitle: "Someone", optionVal: 6 },
+    { optionTitle: "Joe", optionVal: 6 },
+    { optionTitle: "Jacob", optionVal: 6 },
+    { optionTitle: "Andrew", optionVal: 6 },
+    { optionTitle: "Angel", optionVal: 6 }
 ];
 
 export default class PollChart extends React.PureComponent {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      data
-    };
-  }
+        this.state = {
+            data
+        };
+    }
 
-  render() {
-    const { data: chartData } = this.state;
+    render() {
+        const { data: chartData } = this.state;
 
-    return (
-      <Paper>
-        <Chart data={chartData}>
-          <PieSeries
-            valueField="optionVal"
-            argumentField="optionTitle"
-            Title="optionTitle"
-          />
-          <Title text="Poll Results" />
-          <Legend />
-          <Animation />
-        </Chart>
-      </Paper>
-    );
-  }
+        return (
+            <Paper>
+                <Chart data={chartData}>
+                    <PieSeries
+                        valueField="optionVal"
+                        argumentField="optionTitle"
+                        title="optionTitle"
+                    />
+                    <Title text="Poll Results" />
+                    <Legend />
+                    <Animation />
+                </Chart>
+            </Paper>
+        );
+    }
 }
