39b8348e2aa958aa43b8354087ea0bb4e0ec5524
angelpsch <angelpsch@users.noreply.github.com>
2020-08-11 19:37:27 -0500

    lol

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('client/build'));
 
 // Express serve up index.html file if it doesn't recognize route
 const path = require('path');
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
