cb10b289cfe8281c64cd9e116c3769ca4be03eb9
angelpsch <angelpsch@users.noreply.github.com>
2020-08-11 19:52:18 -0500

    meh

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.use("/", authRoutes);
 // Exprees will serve up production assets
 app.use(express.static('client/build'));
 
-// Express serve up index.html file if it doesn't recognize route
-const path = require('path');
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
