44efc81e4f43d857eccb25784a703fb6f3ffc7b9
Jacob Walton <jawalton6616@gmail.com>
2020-08-12 20:44:12 -0500

    fixed push

--- client/src/components/TestComponent/TestComponent.js
+++ client/src/components/TestComponent/TestComponent.js
@@ -2,24 +2,8 @@ import React from "react";
 
 class TestComponent extends Component {
   state = {
-    book: {},
+    poll: {},
   };
-
-  componentDidMount() {
-    API.getBook(this.props.match.params.id)
-      .then((res) => this.setState({ book: res.data }))
-      .catch((err) => console.log(err));
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>
-          {this.state.book.title} by {this.state.book.author}
-        </h1>
-      </div>
-    );
-  }
 }
 
 export default TestComponent;
